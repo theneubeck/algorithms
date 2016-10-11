29c4f927008e5b571af6daf458c74567b041390d
Jens Carlén <jens@neubeck.se>
2016-10-11 21:51:28 +0200

    Working binsearch

---
+++ tests/sortTest.js
@@ -0,0 +1,57 @@
+"use strict";
+require("should");
+
+var mergesort = require("../lib/mergesort");
+var binsearch = require("../lib/binsearch");
+// var _ = require("lodash");
+
+describe("mergesort", () => {
+   
+  [
+    [[], []],
+    [[1], [1]],
+    [[1,2,3], [1,2,3]],
+    [[3,2,1], [1,2,3]],
+    [[5,2,5], [2,5,5]],
+    [[5,2,1,4,6,7,8,9],   [1,2,4,5,6,7,8,9]],    
+    [[9,8,7,6,5,4,3,2,1], [1,2,3,4,5,6,7,8,9]],    
+  ].forEach((solution) => {
+     const [list, expected] = solution;
+     it(`should sort ${list} to ${expected}`, function () {      
+       mergesort(list).should.eql(expected);
+     });
+  });
+
+});
+
+describe("binsearch", () => {
+   
+  [
+    [1, [1], 0],
+    [3, [1,3], 1],
+    [2, [1,2,3], 1],
+    [3, [1,2,3], 2],
+    [7, [1,2,4,5,6,7,8,9], 5],
+    [9, [1,2,3,4,5,6,7,8,9], 8],
+    [2, [1,2,3,4,5,6,7,8,9], 1],
+  ].forEach((solution) => {
+     const [needle, list, index] = solution;
+     it(`should find ${needle} in ${list} at ${index}`, function () {      
+       binsearch(needle, list).should.eql(index);
+     });
+  });
+  [
+    [1, [2], -0],
+    [3, [1,4], -1],
+    [2, [1,3,4], -1],
+    [3, [1,2,4], -2],
+    [7, [1,2,4,5,6,8,9], -5],
+    [9, [1,2,3,4,5,6,7,8], -8],
+    [2, [1,3,4,5,6,7,8,9], -1],
+  ].forEach((solution) => {
+     const [needle, list, index] = solution;
+     it(`should not find ${needle} in ${list} (${index})`, function () {      
+       binsearch(needle, list).should.eql(index);
+     });
+  });
+});
