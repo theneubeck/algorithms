25c7d31e25b94d71b6a087edc3aa06234973b578
Jens Carlén <jens.carlen@offerta.se>
2022-10-30 19:46:51 +0100

    Reset memory between runs

--- tests/dynamic/changeTest.js
+++ tests/dynamic/changeTest.js
@@ -1,7 +1,9 @@
 "use strict";
 require("should");
 const {
-  change
+  change,
+  changeWithMemory,
+  changePreCalc
 } = require("../../lib/dynamic/change");
 // spec: F(n) = F(n-1) + F(n-2), F(0) = 0, F(1) = 1
 
@@ -10,6 +12,14 @@ describe("fibonacci", () => {
   [1000, 500, 100, 50, 20, 10, 5, 1].forEach((number) => {
     it(`should calculate change(${number}) to 1`, () => {
       change(number).should.eql(1);
+    });
+
+    it(`should calculate changeWithMemory(${number}) to 1`, () => {
+      changeWithMemory(number).should.eql(1);
+    });
+
+    it(`should calculate changePreCalc(${number}) to 1`, () => {
+      changePreCalc(number).should.eql(1);
     });
   });
 
@@ -32,5 +42,13 @@ describe("fibonacci", () => {
     it(`should calculate change(${input}) to ${output}`, () => {
       change(input).should.eql(output);
     });
+
+    it(`should calculate changeWithMemory(${input}) to ${output}`, () => {
+      changeWithMemory(input).should.eql(output);
+    });
+    it(`should calculate changePreCalc(${input}) to ${output}`, () => {
+      changePreCalc(input).should.eql(output);
+    });
+
   });
 });
